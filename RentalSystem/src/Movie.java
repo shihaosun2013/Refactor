865ad2d28504c67ba42b4caff63d6a98da6bc1a9
Shihao Sun <shihasun@amazon.com>
2016-06-18 15:02:24 -0700

    Moved getFrequentRenterPoints and getCharge from Rental to Movie class

--- RentalSystem/src/Movie.java
+++ RentalSystem/src/Movie.java
@@ -26,5 +26,34 @@ public class Movie {
         return _title;
     }
 
+    public double getCharge(int daysRented) {
+        double result = 0;
+        switch (getPriceCode()) {
+            case Movie.REGULAR:
+                result += 2;
+                if (daysRented > 2)
+                    result += (daysRented - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                result += daysRented * 3;
+                break;
+            case Movie.CHILDRENS:
+                result += 1.5;
+                if (daysRented > 3)
+                    result += (daysRented - 3) * 1.5;
+                break;
+        }
+        return result;
+    }
+
+    public int getFrequentRenterPoints(int daysRented) {
+        // add frequent renter points
+        // add bonus for a two day new release rental
+        if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1)
+            return 2;
+        else
+            return 1;
+    }
+
 }
 
--- RentalSystem/src/Rental.java
+++ RentalSystem/src/Rental.java
@@ -19,31 +19,10 @@ public class Rental {
     }
 
     public double getCharge() {
-        double result = 0;
-        switch (getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (getDaysRented() > 2)
-                    result += (getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                result += getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if (getDaysRented() > 3)
-                    result += (getDaysRented() - 3) * 1.5;
-                break;
-        }
-        return result;
+        return _movie.getCharge(getDaysRented());
     }
 
     public int getFrequentRenterPoints() {
-        // add frequent renter points
-        // add bonus for a two day new release rental
-        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1)
-            return 2;
-        else
-            return 1;
+        return _movie.getFrequentRenterPoints(getDaysRented());
     }
 }
